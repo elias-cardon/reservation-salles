b04ed52213896dc2488eba1d6d589abb49a987e2
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-06-09 16:33:26 +0200

    Correction d'une partie du css

--- include/header.php
+++ include/header.php
@@ -1,32 +1,25 @@
-<header>
-    <nav id="nav-index">
+<nav id="nav-index">
         <div id="top-title">
+            <a href="index.php"><h3>RESERVATION SALLES</h3></a>
         </div>
-        <a href="index.php">
-            <h3>RESERVATION SALLES</h3></a>
         <?php 
           if(!isset($_SESSION['login'])){
-            echo'<div class="btn"><a href="connexion.php">Se connecter</a></div>
-                 <div class="btn"><a href="inscription.php">S\'inscrire</a></div>';
+            ?>
+            <div class="btn"><a href="connexion.php">Se connecter</a></div>
+            <div class="btn"><a href="inscription.php">S'inscrire</a></div>
+        <?php
           }
         ?>
-        </div>
-    </div>
-</div>
 <?php
 if (isset($_SESSION['login'])) {
-    echo '<nav>
-                <ul class="menu-nav">
-                <div class="btn"><a href="reservation.php">RESERVATION</a></div>
-                <div class="btn"><a href="planning.php">PLANNING</a></div>
-                <div class="btn"><a href="profil.php">PROFIL</a></div>
-                <div class="btn"><a href="logout.php">DECONNEXION</a></div>
-                    </ul> 
-                  </div> 
-                </nav>';
+    ?>
+    <ul class="menu-nav">
+        <li class="btn"><a href="reservation.php">RESERVATION</a></li>
+        <li class="btn"><a href="planning.php">PLANNING</a></li>
+        <li class="btn"><a href="profil.php">PROFIL</a></li>
+        <li class="btn"><a href="logout.php">DECONNEXION</a></li>
+    </ul> 
+    <?php
 }
-        ?>
-   
-      </nav>
-    </div>
-</header>
+?>
+</nav>
