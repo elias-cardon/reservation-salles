ea5ca72190d813ed828aec9d376297bbb5d8f9f9
lea-martel <lea.martel@laplateforme.io>
2020-06-09 20:52:06 +0200

    footer

--- include/footer.php
+++ include/footer.php
@@ -1,2 +1,7 @@
-    <footer>    
-        <p> jfredrji </p>
+    <div id="footer-container">
+        <div id="icone">
+        <a href="https://facebook.com" target="_blank"><i class="fab fa-facebook-f"></i></a>
+        <a href="https://twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
+        <a href="https://www.instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
+      </div>
+</div>
--- index.php
+++ index.php
@@ -10,6 +10,7 @@ if (isset($_POST["deconnexion"])) {
 
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title> Réservation-Salles</title>
 	<meta charset="utf-8">
@@ -18,24 +19,26 @@ if (isset($_POST["deconnexion"])) {
 	<link href="https://fonts.googleapis.com/css2?family=Mukta&display=swap" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="css/index.css">
 </head>
+
 <body>
 	<header>
 		<?php include("include/header.php") ?>
 	</header>
 	<main>
-	<div id="banner">
-		<h2>BIENVENUE<h2>
-		<h4> Trouvez la salle idéal pour vos évènements ! <h4>
-		
-		<form method="post" action="">
-		<input class="recherche" type="search" name="search" placeholder="Nom de l'évènement mariage, anniversaire"> 
-		<input class="recherche" type="search" name="search" placeholder="Où Paris, Marseille">
-		<i class="fas fa-search"></i>
+		<div id="banner">
+			<h2>BIENVENUE<h2>
+					<h4> Trouvez la salle idéal pour vos évènements ! <h4>
+
+							<form method="post" action="">
+								<input class="recherche" type="search" name="search" placeholder="Nom de l'évènement mariage, anniversaire">
+								<input class="recherche" type="search" name="search" placeholder="Où Paris, Marseille">
+								<i class="fas fa-search"></i>
 		</div>
-	</form>
+		</form>
 	</main>
 	<footer>
 		<?php include("include/footer.php") ?>
 	</footer>
 </body>
+
 </html>
