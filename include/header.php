e3a3f40b6809f4010e0d3713aed57fadaf832f60
Bryan Gil <bryan.gil@laplateforme.io>
2020-06-09 12:42:29 +0200

    add

--- include/header.php
+++ include/header.php
@@ -4,24 +4,27 @@
         </div>
         <a href="index.php">
             <h3>RESERVATION SALLES</h3></a>
-        <div class="btn"><a href="connexion.php">Se connecter</a></div>
-        <div class="btn"><a href="inscription.php">S'inscrire</a></div>
+        <?php 
+          if(!isset($_SESSION['login'])){
+            echo'<div class="btn"><a href="connexion.php">Se connecter</a></div>
+                 <div class="btn"><a href="inscription.php">S\'inscrire</a></div>';
+          }
+        ?>
         </div>
     </div>
 </div>
 <?php
-if (isset($_SESSION['login'])){
-          echo '<nav>
+if (isset($_SESSION['login'])) {
+    echo '<nav>
                 <ul class="menu-nav">
-                <li><a href="reservation.php">RESERVATION</a></li>
-                      <li><a href="planning.php">PLANNING</a></li>
-                      <li><a href="profil.php">PROFIL</a></li>
-                      <li><a href="index.php?deco">DECONNEXION</a></li>
+                <div class="btn"><a href="reservation.php">RESERVATION</a></div>
+                <div class="btn"><a href="planning.php">PLANNING</a></div>
+                <div class="btn"><a href="profil.php">PROFIL</a></div>
+                <div class="btn"><a href="index.php?deco">DECONNEXION</a></div>
                     </ul> 
                   </div> 
-                </nav>'; 
-       
-          }
+                </nav>';
+}
         ?>
    
       </nav>
