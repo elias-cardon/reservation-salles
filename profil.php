10043bbab5c8ba1a9038862d2b7080c0ee286b2d
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-06-09 13:36:13 +0200

    Mise du CSS au profil

--- profil.php
+++ profil.php
@@ -1,32 +1,42 @@
-<?php
-session_start();
-if ($_SESSION['login']) {
-	echo "<p>Bienvenue ".$_SESSION['login']. " ! <br/><br/>
+<?php session_start();
 
-	<a href='changement_mdp.php'>Changer de mot de passe</a><br/>
-
-	<a href='changement_login.php'>Changer de login</a><br/>
-	<a href='logout.php'>Se déconnecter</a></p>";
-}
-else
-{
-	header("Location:connexion.php");
+if (isset($_POST["deconnexion"])) {
+	session_unset();
+	session_destroy();
+	header('Location:index.php');
 }
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Profil</title>
+	<link rel="stylesheet" type="text/css" href="css/profil.css">
 </head>
 <body>
 	<!-- Header -->
-			<header id="header">
-				<ul id="lien">
-					<li><a href="index.php">Accueil</a></li>
-				</ul>
-			</header>
-			<!-- Footer -->
-			<footer id="footer">
-			</footer>
+	<header>
+		<?php include("include/header.php") ?>
+	</header>
+	<main>
+		<div class="milieu">
+			<?php
+			if ($_SESSION['login']) {
+				echo "<p>Bienvenue ".$_SESSION['login']. " ! <br/><br/>
+
+				<a href='changement_mdp.php'>Changer de mot de passe</a><br/>
+
+				<a href='changement_login.php'>Changer de login</a><br/>
+
+				<a href='logout.php'>Se déconnecter</a></p>";
+				}
+			else
+			{
+				header("Location:connexion.php");
+			}
+		?>
+		</div>
+		
+	</main>
 </body>
 </html>
