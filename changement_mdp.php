e3f8ab4201e19e27663f2e0497c958bb12d29c83
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-06-10 21:42:08 +0200

    Mise en forme des codes

--- changement_mdp.php
+++ changement_mdp.php
@@ -1,79 +1,72 @@
 <?php session_start();
 
 if (isset($_POST["deconnexion"])) {
-	session_unset();
-	session_destroy();
-	header('Location:index.php');
+    session_unset();
+    session_destroy();
+    header('Location:index.php');
 }
 ?>
 
 <?php
 if (isset($_SESSION['login'])) {
-	$username = $_SESSION['login'];
-	if (isset($_POST['submit'])) {
-		$password = $_POST['password'];
-		$newpassword = $_POST['newpassword'];
-		$repeatnewpassword = $_POST['repeatnewpassword'];
-		if ($password && $newpassword && $repeatnewpassword) {
-			if ($newpassword == $repeatnewpassword) {
-				$db = mysqli_connect('localhost', 'root', '') or die('Erreur');
-				mysqli_select_db($db, 'reservationsalles');
-				$query = mysqli_query($db, "SELECT * FROM utilisateurs WHERE login = '$username' AND password = '$password'");
-				$rows = mysqli_num_rows($query);
-				if ($rows==1) {
-					$newpass = mysqli_query($db, "UPDATE utilisateurs SET password='$newpassword' WHERE login='$username'");
-					die("Votre mot de passe a bien été modifié. Vous pouvez vous <a href='connexion.php'>connecter</a>.");
-				}
-				else
-				{
-					echo "Votre ancien mot de passe est incorrect";
-				}
-			}
-			else
-				{echo "Les deux champs doivent être identiques";}
-		
-		}
-		else
-		{
-			echo "Veuillez saisir tous les champs";
-		}
-	}
+    $username = $_SESSION['login'];
+    if (isset($_POST['submit'])) {
+        $password = $_POST['password'];
+        $newpassword = $_POST['newpassword'];
+        $repeatnewpassword = $_POST['repeatnewpassword'];
+        if ($password && $newpassword && $repeatnewpassword) {
+            if ($newpassword == $repeatnewpassword) {
+                $db = mysqli_connect('localhost', 'root', '') or die('Erreur');
+                mysqli_select_db($db, 'reservationsalles');
+                $query = mysqli_query($db, "SELECT * FROM utilisateurs WHERE login = '$username' AND password = '$password'");
+                $rows = mysqli_num_rows($query);
+                if ($rows == 1) {
+                    $newpass = mysqli_query($db, "UPDATE utilisateurs SET password='$newpassword' WHERE login='$username'");
+                    die("Votre mot de passe a bien été modifié. Vous pouvez vous <a href='connexion.php'>connecter</a>.");
+                } else {
+                    echo "Votre ancien mot de passe est incorrect";
+                }
+            } else {
+                echo "Les deux champs doivent être identiques";
+            }
+
+        } else {
+            echo "Veuillez saisir tous les champs";
+        }
+    }
 
 
-
-echo '
+    echo '
 	';
-}
-else
-{
-	header("Location:connexion.php");
+} else {
+    header("Location:connexion.php");
 }
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Changement de login</title>
-	<link rel="stylesheet" type="text/css" href="css/index.css">
-	<link rel="stylesheet" type="text/css" href="css/changement_mdp.css">
-	<script src="https://kit.fontawesome.com/5a25ce672a.js" crossorigin="anonymous"></script>
+    <title>Changement de login</title>
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="stylesheet" type="text/css" href="css/changement_mdp.css">
+    <script src="https://kit.fontawesome.com/5a25ce672a.js" crossorigin="anonymous"></script>
 </head>
 <body>
-	<!-- Header -->
-			<header>
-				<?php include("include/header.php") ?>
-			</header>
-			<!-- Main -->
-			<main>
-				<form method="POST" action="changement_mdp.php">
-					<p>Votre ancien mot de passe</p>
-					<input class="input" type="password" name="password"></input>
-					<p>Votre nouveau mot de passe</p>
-					<input class="input" type="password" name="newpassword"></input>
-					<p>Répétez votre nouveau mot de passe</p>
-					<input class="input" type="password" name="repeatnewpassword"></input>
-					<input class="input" type="submit" value="Submit" name="submit"></input>
-				</form>
-			</main>
+<!-- Header -->
+<header>
+    <?php include("include/header.php") ?>
+</header>
+<!-- Main -->
+<main>
+    <form method="POST" action="changement_mdp.php">
+        <p>Votre ancien mot de passe</p>
+        <input class="input" type="password" name="password"></input>
+        <p>Votre nouveau mot de passe</p>
+        <input class="input" type="password" name="newpassword"></input>
+        <p>Répétez votre nouveau mot de passe</p>
+        <input class="input" type="password" name="repeatnewpassword"></input>
+        <input class="input" type="submit" value="Submit" name="submit"></input>
+    </form>
+</main>
 </body>
 </html>
--- include/header.php
+++ include/header.php
@@ -1,25 +1,25 @@
 <nav id="nav-index">
-        <div id="top-title">
-            <a href="index.php"><h3>RESERVATION SALLES</h3></a>
-        </div>
-        <?php 
-          if(!isset($_SESSION['login'])){
-            ?>
-            <div class="btn"><a href="connexion.php">Se connecter</a></div>
-            <div class="btn"><a href="inscription.php">S'inscrire</a></div>
-        <?php
-          }
-        ?>
-<?php
-if (isset($_SESSION['login'])) {
-    ?>
-    <ul class="menu-nav">
-        <li class="btn"><a href="reservation-form.php">RESERVATION</a></li>
-        <li class="btn"><a href="planning.php">PLANNING</a></li>
-        <li class="btn"><a href="profil.php">PROFIL</a></li>
-        <li class="btn"><a href="logout.php">DECONNEXION</a></li>
-    </ul> 
+    <div id="top-title">
+        <a href="index.php"><h3>RESERVATION SALLES</h3></a>
+    </div>
     <?php
-}
-?>
+    if (!isset($_SESSION['login'])) {
+        ?>
+        <div class="btn"><a href="connexion.php">Se connecter</a></div>
+        <div class="btn"><a href="inscription.php">S'inscrire</a></div>
+        <?php
+    }
+    ?>
+    <?php
+    if (isset($_SESSION['login'])) {
+        ?>
+        <ul class="menu-nav">
+            <li class="btn"><a href="reservation-form.php">RESERVATION</a></li>
+            <li class="btn"><a href="planning.php">PLANNING</a></li>
+            <li class="btn"><a href="profil.php">PROFIL</a></li>
+            <li class="btn"><a href="logout.php">DECONNEXION</a></li>
+        </ul>
+        <?php
+    }
+    ?>
 </nav>
