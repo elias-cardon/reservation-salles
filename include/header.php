03570e41da6e8e488420303dce1cc20a24c646d5
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-06-10 10:35:25 +0200

    Update header.php

--- include/header.php
+++ include/header.php
@@ -1,23 +1,25 @@
-<header>
-    <nav id="nav-index">
+<nav id="nav-index">
         <div id="top-title">
-          <a href="index.php"><h3>RESERVATION SALLES</h3></a>
+            <a href="index.php"><h3>RESERVATION SALLES</h3></a>
         </div>
-        <div class="btn"><a href="connexion.php">Se connecter</a></div>
-        <div class="btn"><a href="inscription.php">S'inscrire</a></div>
-<?php
-if (isset($_SESSION['login'])){
-          echo '<nav>
-                <ul class="menu-nav">
-                <li><a href="reservation.php">RESERVATION</a></li>
-                      <li><a href="planning.php">PLANNING</a></li>
-                      <li><a href="profil.php">PROFIL</a></li>
-                      <li><a href="logout.php">DECONNEXION</a></li>
-                    </ul> 
-                  </div> 
-                </nav>';
-       
+        <?php 
+          if(!isset($_SESSION['login'])){
+            ?>
+            <div class="btn"><a href="connexion.php">Se connecter</a></div>
+            <div class="btn"><a href="inscription.php">S'inscrire</a></div>
+        <?php
           }
         ?>
-
-</header>
+<?php
+if (isset($_SESSION['login'])) {
+    ?>
+    <ul class="menu-nav">
+        <li class="btn"><a href="reservation.php">RESERVATION</a></li>
+        <li class="btn"><a href="planning.php">PLANNING</a></li>
+        <li class="btn"><a href="profil.php">PROFIL</a></li>
+        <li class="btn"><a href="logout.php">DECONNEXION</a></li>
+    </ul> 
+    <?php
+}
+?>
+</nav>
