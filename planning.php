c3df2b485c18d205209fc1c063fd1974c523c44e
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-06-10 11:31:26 +0200

    MàJ

---
+++ planning.php
@@ -0,0 +1,92 @@
+<?php session_start();
+
+if (isset($_POST["deconnexion"])) {
+    session_unset();
+    session_destroy();
+    header('Location:index.php');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Planning</title>
+    <link rel="stylesheet" href="css/planning.css">
+    <link rel="stylesheet" href="css/index.css">
+
+</head>
+<body>
+    <!--HEADER-->
+    <header>
+        <?php include('include/header.php') ?>
+    </header>
+    
+
+    <!--MAIN-->
+    <main>
+        <table>
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>Lundi</th>
+
+                    <th>Mardi</th>
+
+                    <th>Mercredi</th>
+
+                    <th>Jeudi</th>
+
+                    <th>Vendredi</th>
+
+                    <th>Samedi</th>
+
+                    <th>Dimanche</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>8:00</td>
+                </tr>
+                <tr>
+                    <td>9:00</td>
+                </tr>
+                <tr>
+                    <td>10:00</td>
+                </tr>
+                <tr>
+                    <td>11:00</td>
+                </tr>
+                <tr>
+                    <td>12:00</td>
+                </tr>
+                <tr>
+                    <td>13:00</td>
+                </tr>
+                <tr>
+                    <td>14:00</td>
+                </tr>
+                <tr>
+                    <td>15:00</td>
+                </tr>
+                <tr>
+                    <td>16:00</td>
+                </tr>
+                <tr>
+                    <td>17:00</td>
+                </tr>
+                <tr>
+                    <td>18:00</td>
+                </tr>
+                <tr>
+                    <td>19:00</td>
+                </tr>
+            </tbody>
+    
+    
+        </table>
+    </main>
+    
+</body>
+</html>
