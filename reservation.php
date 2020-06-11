56f4a9a404d5b7006fb7d2a5dc671e8573a659c3
lea-martel <lea.martel@laplateforme.io>
2020-06-11 12:31:05 +0200

    reservation requete

---
+++ reservation.php
@@ -0,0 +1,19 @@
+<?php session_start();
+$bdd = mysqli_connect("localhost", "root", "", 'reservationsalles');
+
+if (isset($_GET['submit'])) {
+
+$login = $_GET['login'];
+$requete = "SELECT id FROM utilisateurs WHERE login ='".$_SESSION['login']."'";
+$query = mysqli_query($bdd, $requete);
+$id = mysqli_fetch_all($query);
+
+
+$requete1 = "SELECT titre, description, FROM reservations";
+$query1 = mysqli_query($bdd,$requete1);
+$reserv = mysqli_fetch_all($query1);
+
+
+
+} 
+
