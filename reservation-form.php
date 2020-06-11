7909fbc9560c7d54a654512e766eda223b546abc
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-06-11 13:02:26 +0200

    Update reservation-form.php

--- reservation-form.php
+++ reservation-form.php
@@ -23,21 +23,21 @@ if (isset($_POST["deconnexion"])) {
 
 <!-- Main -->
 <main>
-    <form method="POST" action="changement_login.php">
+    <form method="POST" action="planning.php">
         <label for="name">Titre</label><br>
         <input type="text" id="name" name="name" required
                minlength="4" maxlength="140" size="10"><br>
         <label for="descrip">Description</label><br>
         <input type="text" id="name" name="name" required
                minlength="4" maxlength="255" size="10"><br>
-        <label for="date_deb">Date de début</label><br>
-        <input type="date" id="start" name="trip-start"
-               value="2018-07-22"
-               min="2020-01-01" max="2100-12-31"><br>
-        <label for="date_fin">Date de fin</label><br>
-        <input type="date" id="start" name="trip-start"
-               value="2018-07-22"
-               min="2020-01-01" max="2100-12-31"><br>
+        <label>Horaire début :</label>
+        <input type="date" name="datedeb" required>
+        <input type="time" name="timedeb" value="08:00" step="3600" min="08:00" max="18:00"
+               required><br>
+        <label>Horaire fin :</label>
+        <input type="date" name="datefin" required>
+        <input type="time" name="timefin" value="09:00" step="3600" min="09:00" max="19:00"
+               required><br>
         <input class="input" type="submit" name="submit" value="Valider">
     </form>
 </main>
