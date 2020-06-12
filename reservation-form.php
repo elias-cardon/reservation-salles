df0656976c89ebc88a88a8dce53a442dab5c4127
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-06-12 10:47:58 +0200

    Update reservation-form.php

--- reservation-form.php
+++ reservation-form.php
@@ -2,17 +2,17 @@
 session_start();
 $bdd = mysqli_connect("localhost", "root", "", 'reservationsalles');
 
-    if (isset($_POST['submit'])) {
+if (isset($_POST['submit'])) {
 
     //Variables
     $titre = htmlspecialchars($_POST['titre']);
     $description = htmlspecialchars($_POST['description']);
-    $debut = htmlspecialchars($_POST['date-debut']). " ".$_POST['heure-debut'];
-    $fin = htmlspecialchars($_POST['date-fin']). " ".$_POST['heure-fin'];
+    $debut = htmlspecialchars($_POST['date-debut']) . " " . $_POST['heure-debut'];
+    $fin = htmlspecialchars($_POST['date-fin']) . " " . $_POST['heure-fin'];
     $date = htmlspecialchars($_POST['date-debut']);
 
 
-    $requete = "SELECT id FROM utilisateurs WHERE login ='".$_SESSION['login']."'";
+    $requete = "SELECT id FROM utilisateurs WHERE login ='" . $_SESSION['login'] . "'";
     $query = mysqli_query($bdd, $requete);
     $id = mysqli_fetch_all($query);
     $id_utilisateur = $id[0][0];
@@ -22,8 +22,8 @@ $bdd = mysqli_connect("localhost", "root", "", 'reservationsalles');
 
     var_dump($debut);
     var_dump($fin);
-    $requete2 = "INSERT INTO reservations (titre, description, debut, fin, id_utilisateur) VALUES ('$titre', '$description', '$debut', '$fin', $id_utilisateur)";
-    $query1 = mysqli_query($bdd,$requete2);
+    $requete2 = "INSERT INTO reservations (titre, description, debut, fin, id_utilisateur) VALUES ('$titre', '$description', '$debut', '$fin', '$id_utilisateur')";
+    $query1 = mysqli_query($bdd, $requete2);
     var_dump($query1);
 
 }
@@ -41,31 +41,31 @@ $bdd = mysqli_connect("localhost", "root", "", 'reservationsalles');
 </head>
 <body>
 
-    <!--Header-->
-    <header>
-    <?php include('include/header.php');?>
-    </header>
+<!--Header-->
+<header>
+    <?php include('include/header.php'); ?>
+</header>
 
-     <!--Main-->
-     <div class="center-body">
+<!--Main-->
+<div class="center-body">
     <form action="#" method="post">
 
-    <label for="titre">Titre:</label><br />
-        <input type="text" name="titre"><br />
-    <label for="description">Description:</label><br />
-        <textarea id="description" name="description" ></textarea><br />
-    <label for="debut">Début:</label><br />
-        <input type="date" name="date-debut"><br />
-    <label for="fin">Fin:</label><br />
-        <input type="date" name="date-fin"><br /><br />
-    <label for="heure">Heure:</label><br />
-        <input type="time" name="heure-debut"><br /><br />
-    <label for="heure">Heure:</label><br />
-        <input type="time" name="heure-fin"><br /><br />
+        <label for="titre">Titre:</label><br/>
+        <input type="text" name="titre"><br/>
+        <label for="description">Description:</label><br/>
+        <textarea id="description" name="description"></textarea><br/>
+        <label for="debut">Début:</label><br/>
+        <input type="date" name="date-debut"><br/>
+        <label for="fin">Fin:</label><br/>
+        <input type="date" name="date-fin"><br/><br/>
+        <label for="heure">Heure:</label><br/>
+        <input type="time" name="heure-debut"><br/><br/>
+        <label for="heure">Heure:</label><br/>
+        <input type="time" name="heure-fin"><br/><br/>
 
-    <input type="submit" name="submit" value="Réserver">
+        <input type="submit" name="submit" value="Réserver">
     </form>
-    </div>
+</div>
 
 </body>
 </html>
