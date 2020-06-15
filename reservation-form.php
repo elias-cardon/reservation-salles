a29029437201dd51eb521152b39f2b731f525c54
lea-martel <lea.martel@laplateforme.io>
2020-06-15 12:47:31 +0200

    bdd

--- reservation-form.php
+++ reservation-form.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$bdd = new PDO('mysql:host=localhost;dbname=reservationsalles;charset=utf8', 'root', '' );
+$bdd = mysqli_connect("localhost", "root", "", 'reservationsalles');
 
 if (isset($_POST['submit'])) {
 
@@ -8,12 +8,7 @@ if (isset($_POST['submit'])) {
     $titre = htmlspecialchars($_POST['titre']);
     $description = htmlspecialchars($_POST['description']);
     $debut = htmlspecialchars($_POST['date-debut']). " ".$_POST['heure-debut'];
-    $fin = htmlspecialchars($_POST['date-fin']). " ".$_POST['heure-fin'];
-    
-
-    $requete = $bdd->query("SELECT id FROM utilisateurs WHERE login ='".$_SESSION['login']."'");//concatenation no comprendo
-    $id = $requete->fetchAll();
-    $id_utilisateur = $id[0][0]; //c'est comment 
+    $fin = htmlspecialchars($_POST['date-fin']). " ".$_POST['heure-fin']; 
 
     $requete = "SELECT id FROM utilisateurs WHERE login ='" . $_SESSION['login'] . "'";
     $query = mysqli_query($bdd, $requete);
