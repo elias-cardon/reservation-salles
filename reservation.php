3d6ac5b42ca9937d2a4053b3689e66b1b5043360
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-06-15 11:58:07 +0200

    Update reservation.php

--- reservation.php
+++ reservation.php
@@ -23,6 +23,24 @@ foreach($id as $value){
 }
 ?>
 
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Planning</title>
+    <link rel="stylesheet" href="css/planning.css">
+    <link rel="stylesheet" href="css/index.css">
+
+</head>
+<body>
+<!--HEADER-->
+<header>
+    <?php include('include/header.php') ?>
+</header>
+</body>
+</html>
+
 <style>
 
     table{
@@ -34,10 +52,3 @@ foreach($id as $value){
     border:1px solid black;
 }
 </style>
-
-
-
-
-
-
-
