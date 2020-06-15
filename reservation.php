bf4e1b928eb66ad01aaa4c070782320b60853805
Bryan Gil <bryan.gil@laplateforme.io>
2020-06-15 11:50:57 +0200

    reservation.php

--- reservation.php
+++ reservation.php
@@ -1,19 +1,43 @@
 <?php session_start();
 $bdd = mysqli_connect("localhost", "root", "", 'reservationsalles');
 
-if (isset($_GET['submit'])) {
+//if (isset($_GET['submit'])) {
 
-$login = $_GET['login'];
-$requete = "SELECT id FROM utilisateurs WHERE login ='".$_SESSION['login']."'";
+//$login = $_GET['login'];
+$requete = "SELECT * FROM utilisateurs INNER JOIN reservations ON utilisateurs.id = reservations.id_utilisateur ";
 $query = mysqli_query($bdd, $requete);
 $id = mysqli_fetch_all($query);
 
+echo "<table>";
 
-$requete1 = "SELECT titre, description, FROM reservations";
-$query1 = mysqli_query($bdd,$requete1);
-$reserv = mysqli_fetch_all($query1);
+echo "<tr>";
+    echo "<th>"."Login"."</th>";
+    echo "<th>"."Titre"."</th>";
+    echo "<th>"."Description"."</th>";
+foreach($id as $value){
+    echo "<tr>";
+        echo "<td>"."$value[2]"."</td>";
+        echo "<td>"."$value[4]"."</td>";
+        echo "<td>"."$value[5]"."</td>";
+    echo "</tr>";
+}
+?>
 
-$requete2 = "";
+<style>
 
-} 
+    table{
+        border-collapse:collapse;
+        margin: auto;
+    }
+
+    td, th{
+    border:1px solid black;
+}
+</style>
+
+
+
+
+
+
 
