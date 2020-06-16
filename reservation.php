d2ce3e1e26a7760f93f32a127a85c0a3170accea
Bryan Gil <bryan.gil@laplateforme.io>
2020-06-16 18:36:00 +0200

    test

--- reservation.php
+++ reservation.php
@@ -21,23 +21,28 @@ session_start();
 <?php
 $bdd = mysqli_connect("localhost", "root", "", 'reservationsalles');
 
-$requete = "SELECT * FROM utilisateurs INNER JOIN reservations ON utilisateurs.id = reservations.id_utilisateur";
+$requete = "SELECT login,titre,description,debut,fin FROM utilisateurs INNER JOIN reservations ON utilisateurs.id = reservations.id_utilisateur WHERE login ='" . $_SESSION['login'] . "' LIMIT 0,1";
 $query = mysqli_query($bdd, $requete);
 $id = mysqli_fetch_all($query);
-$id_reservation = $_GET['id'];
 
 echo "<div class=\"center\">";
+
 echo "<table>";
 
 echo "<tr>";
 echo "<th>" . "Login" . "</th>";
 echo "<th>" . "Titre" . "</th>";
 echo "<th>" . "Description" . "</th>";
+echo "<th>" . "Début" . "</th>";
+echo "<th>" . "Fin" . "</th>";
+
 foreach ($id as $value) {
     echo "<tr>";
+    echo "<td>" . "$value[0]" . "</td>";
+    echo "<td>" . "$value[1]" . "</td>";
     echo "<td>" . "$value[2]" . "</td>";
+    echo "<td>" . "$value[3]" . "</td>";
     echo "<td>" . "$value[4]" . "</td>";
-    echo "<td>" . "$value[5]" . "</td>";
     echo "</tr>";
 }
 echo "</table>";
@@ -65,9 +70,41 @@ echo "</div>";
         align-items: center;
         height: 100%;
     }
+    /*TABLE*/
+
+    table {
+
+        width: 60%;
+        background: #dfdddd6b;
+        border-collapse: collapse;
+    }
+
+    table, td, th {
+        border: 1px solid rgba(185, 185, 185, 0.397);
+
+    }
+
+    th {
+
+
+        padding: 10px;
+        font-size: 20px;
+        color: #fff;
+        text-transform: uppercase;
+        background-color: rgb(20, 66, 68);
+
+
+    }
+
+    td {
+        text-align: center;
+        padding: 10px;
+        width: 500px;
+        height: 45px;
+    }
+
+    td:hover {
+        box-shadow: 300px 0 0 0 pink inset;
+    }
+
 </style>
-
-
-
-
-
