bf6306bcb37eba6b5e8d29a10268fb4063ba161e
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-06-17 10:36:02 +0200

    Update reservation.php

--- reservation.php
+++ reservation.php
@@ -19,24 +19,38 @@ session_start();
 </header>
 </body>
 </html>
-<main>
-    <?php
-    $connect = mysqli_connect('localhost','root','', 'reservationsalles');
-    $request = "SELECT * FROM reservations WHERE id = '".$_GET['id']."'";
-    $query = mysqli_query($connect, $request);
-    $result = mysqli_fetch_assoc($query);
+<?php
+//A REVOIR
 
-    $requestuser = "SELECT login FROM utilisateurs WHERE id = '".$result['id_utilisateur']."'";
-    $queryuser = mysqli_query($connect, $requestuser);
-    $resultuser = mysqli_fetch_assoc($queryuser);
-    ?>
-    <div id="reservationdiv">
-        <div class="reservationecho"><?php echo $result['titre']; ?></div>
-        <div class="reservationecho">Réserver par : <?php echo $resultuser['login']; ?></div>
-        <div class="reservationecho">Du : <?php echo $result['debut']; ?></div>
-        <div class="reservationecho">Au : <?php echo $result['fin']; ?></div>
-        <div class="reservationecho">Description : <br/><?php echo $result['description']; ?></div>
-    </div>
-</main>
-</body>
-</html>
+$bdd = mysqli_connect("localhost", "root", "", 'reservationsalles');
+//A REVOIR
+
+//RECUP AVEC GET
+$id_get = "SELECT id_utilisateur FROM reservations";
+$query1 = mysqli_query($bdd, $id_get);
+$result1 = mysqli_fetch_assoc($query1);
+
+
+echo "<div class=\"center\">";
+
+echo "<table>";
+
+echo "<tr>";
+echo "<th>" . "Login" . "</th>";
+echo "<th>" . "Titre" . "</th>";
+echo "<th>" . "Description" . "</th>";
+echo "<th>" . "Début" . "</th>";
+echo "<th>" . "Fin" . "</th>";
+
+foreach ($id as $value) {
+    echo "<tr>";
+    echo "<td>" . "$value[0]" . "</td>";
+    echo "<td>" . "$value[1]" . "</td>";
+    echo "<td>" . "$value[2]" . "</td>";
+    echo "<td>" . "$value[3]" . "</td>";
+    echo "<td>" . "$value[4]" . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+echo "</div>";
+?>
