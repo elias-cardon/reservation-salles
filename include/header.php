a1a058493ecf235189af98667b93e3c3eb75fee3
Bryan Gil <bryan.gil@laplateforme.io>
2020-06-21 17:57:41 +0200

    ajustement responsive .btn_center inline index.css

--- include/header.php
+++ include/header.php
@@ -5,20 +5,25 @@
         <?php 
           if(!isset($_SESSION['login'])){
             ?>
+            <div class="btn_center">
             <div class="btn"><a href="connexion.php">Se connecter</a></div>
             <div class="btn"><a href="inscription.php">S'inscrire</a></div>
+            </div>
         <?php
           }
         ?>
 <?php
 if (isset($_SESSION['login'])) {
     ?>
+    
+    <div class="btn_center">
     <ul class="menu-nav">
         <li class="btn"><a href="reservation-form.php">RESERVATION</a></li>
         <li class="btn"><a href="planning.php">PLANNING</a></li>
         <li class="btn"><a href="profil.php">PROFIL</a></li>
         <li class="btn"><a href="logout.php">DECONNEXION</a></li>
     </ul> 
+    </div>
     <?php
 }
 ?>
